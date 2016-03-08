4091b0dc21c8d7a942f47b41e41aac1142b1b09a
Derek Sakamoto <dmsakamoto@gmail.com>
2016-03-08 11:52:51 -0800

    Added Clever credentials to server configuration

--- server/index.js
+++ server/index.js
@@ -16,6 +16,16 @@ let config = {
   env: process.env.NODE_ENV || 'development'
 }
 
+// Load Clever ID and Secret for respective environment
+if (config.env === 'development') {
+  let secrets = require('./secrets');
+  config.clever.id = secrets.clever.id;
+  config.clever.secret = secrets.clever.secret;
+} else {
+  config.clever.id = process.env.CLEVER_ID;
+  config.clever.secret = process.env.CLEVER_SECRET;
+}
+
 // Load middleware
 app.use('/images', express.static(__dirname + '/../public/images'));
 
