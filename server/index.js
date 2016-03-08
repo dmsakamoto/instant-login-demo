3d1127d5786d21230aba10ea66ffe32ba1046522
Derek Sakamoto <dmsakamoto@gmail.com>
2016-03-08 11:46:20 -0800

    Created initial app for registration

--- server/index.js
+++ server/index.js
@@ -1,49 +1,32 @@
-
+/**
+ * index.js - Main server file that we will use for our demo app
+ */
 
 'use strict'
 
 // Load dependencies
 let express = require('express');
-let session = require('express-session');
-let morgan = require('morgan');
-let bodyParser = require('bodyParser');
-let path = require('path');
-// Local dependencies
-// let api = require('./api');
-let oauth = require('./oauth');
-let config = require('./config');
 
-// Set up server and global middleware
+// Set up server
 let app = express();
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(session({
-  'secret': 'somesecret',
-  'resave': false,
-  'saveUnitialized': true,
-  'cookie': { 'secure': true }
-}));
 
-// This is the route to the OAuth Middleware
-app.use('/oauth', oauth);
-
-// This sets up hotloading
-if (config.hotloading) {
-  let webpack = require('webpack');
-  let webpackDevMiddleware = require('webpack-dev-middleware');
-  let webpackHotMiddleware = require('webpack-hot-middleware');
-  let webpackConfig = require('../webpack.config');
-  let compiler = webpack(webpackConfig);
-  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }));
-  app.use(webpackHotMiddleware(compiler));
+// Set configuration variables
+let config = {
+  port: process.env.PORT || 3000,
+  env: process.env.NODE_ENV || 'development'
 }
 
-// app.use('/api', api);
+// Load middleware
+app.use('/images', express.static(__dirname + '/../public/images'));
 
+// Set up initial routes and requests
 app.get('/', function (req, res) {
   res.sendFile('index.html', { root: __dirname + '/../public' });
 });
+
+app.get('/app', function (req, res) {
+  res.sendFile('app.html', { root: __dirname + '/../public' });
+})
 
 app.listen(config.port);
 console.log('Listening on http://localhost:' + config.port);
